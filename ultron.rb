26b81307d5fe99595852913828b6a154bc2630eb
Edwin Hermans <edwin@madtech.cx>
2015-11-20 16:09:49 +0100

    Rename script to new bot name

--- ultron.rb
+++ ultron.rb
@@ -2,16 +2,17 @@ require 'cinch'
 
 # plugins
 require_relative "plugins/hello"
+require_relative "plugins/version"
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.nick = "ultron"
     c.server = "irc.freenode.org"
     c.channels = ["##ultron", "#dehaarlemseconnectie"]
-    c.realname = "The Paranoid Android"
+    c.realname = "I will survice, Hey Hey"
     c.user = "ultron"
     c.plugins.prefix = /^~/
-    c.plugins.plugins = [Hello]
+    c.plugins.plugins = [Hello, Version]
   end
 
 end
