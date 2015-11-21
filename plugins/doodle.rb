0b93a75c5f6ecfef737dfeaefd14c6f28523a333
Edwin Hermans <edwin@madtech.cx>
2015-11-21 01:10:01 +0100

    Refactor but not safely

--- plugins/doodle.rb
+++ plugins/doodle.rb
@@ -1,17 +1,20 @@
 require_relative 'lib/doodle'
 
+# Create instance of DoodlePoll class and execute command
 class Doodle
   include Cinch::Plugin
 
-  match(/doodle (.*)/)
+  match(/doodle(.*?)/)
 
-  def execute(msg, text)
-    if text
-      url = text
-    else
-      url = msg.channel.topic
+  def execute(msg, query)
+    if query
+      query_parts = query.split
+      cmd = query.parts[0]
+      url = query.parts[1] if query_parts > 1
     end
+    cmd = 'winner' unless cmd
+    url = %r{http://doodle.com/polls/.+?}.match(msg.channel.topic)[0] unless url
     doodle = DoodlePoll.new(url)
-    msg.reply doodle.winner
+    msg.reply doodle.send(cmd)
   end
 end
