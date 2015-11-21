64aea4425a91f2dfeb1b6b7f2cb59727ac28ff56
Edwin Hermans <edwin@madtech.cx>
2015-11-21 01:29:09 +0100

    Add automatic nickserv auth; read password from config file

--- ultron.rb
+++ ultron.rb
@@ -1,23 +1,31 @@
 require 'cinch'
+require 'yaml'
 
 # plugins
-require_relative "plugins/hello"
-require_relative "plugins/version"
-require_relative "plugins/source"
-require_relative "plugins/doodle"
+require_relative 'plugins/identify'
+require_relative 'plugins/hello'
+require_relative 'plugins/version'
+require_relative 'plugins/source'
+require_relative 'plugins/doodle'
+
+config = YAML.load(File.read('./config.yml'))
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick = "ultron"
-    c.server = "irc.freenode.org"
-    c.channels = ["##ultron", "#dehaarlemseconnectie"]
-    c.realname = "I will survice, Hey Hey"
-    c.user = "ultron"
+    c.nick = 'ultron'
+    c.server = 'irc.freenode.org'
+    c.channels = ['##ultron', '#dehaarlemseconnectie']
+    c.realname = 'I will survice, Hey Hey'
+    c.user = 'ultron'
     c.plugins.prefix = /^~/
-    c.plugins.plugins = [Hello, Version, Source]
+    c.plugins.plugins = [Identify, Hello, Version, Source]
+    c.plugins.options[Identify] = {
+      password: config['password'],
+      type:     :nickserv
+    }
   end
 
-  trap "SIGINT" do
+  trap 'SIGINT' do
     bot.quit
   end
 end
